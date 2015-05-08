11ac8e779964f637cb928c6368ab9b430f54d547
Kristopher Clemente <kclemente@LA-KCLEMENTE-MBP15.local>
2015-05-08 12:25:09 -0700

    Adding more test cases.  Adding a try/except block to deal with invalid json.

--- img_count_test.py
+++ img_count_test.py
@@ -1,14 +1,45 @@
 import unittest
 import json
+import requests
+import sys
 
 class ImageCountTestCases(unittest.TestCase):
 
     IMG_COUNT_FILE = 'img_count_output.txt'
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         file = open(self.IMG_COUNT_FILE)
-        self.json_dict = json.loads(file.read())
+        try:
+            self.json_dict = json.loads(file.read())
+        except ValueError, e:
+            print self.IMG_COUNT_FILE + " is not valid JSON!"
+            print e
+            sys.exit(2)
         file.close()
+
+    def testJsonKeys(self):
+        """
+            Desciprtion:    Test to ensure that all required keys and only those key exist each JSON block.
+            Details:    Loops through each entry and checks to see if url, count and imdb_id is in the keys.
+                        Also checks to see that each JSON has exactly 3 entries.
+        """
+        incorrect_keys = []
+        correct_keys = [
+            'url',
+            'count',
+            'imdb_id'
+        ]
+        for el in self.json_dict:
+            if len(el) != 3:
+                incorrect_keys.append(el)
+            else:
+                for key in correct_keys:
+                    if key not in el:
+                        incorrect_keys.append(el)
+        self.assertEquals(incorrect_keys,
+                            [],
+                            msg="The following dictionaries did not have the correct keys:\n" + str(incorrect_keys))
 
     def testImdbIdInURL(self):
         """
@@ -18,9 +49,13 @@ class ImageCountTestCases(unittest.TestCase):
         """
         imdb_id_url_mismatches = []
         for el in self.json_dict:
-            if 'http://www.imdb.com/title/tt' + el['imdb_id'] != el['url']:
+            if 'imdb_id' not in el:
                 imdb_id_url_mismatches.append(el)
-        self.assertEquals(imdb_id_url_mismatches,[])
+            elif 'http://www.imdb.com/title/tt' + el['imdb_id'] != el['url']:
+                imdb_id_url_mismatches.append(el)
+        self.assertEquals(imdb_id_url_mismatches,
+                            [],
+                            "The following dictionaries had a mismatch between the url and imdb_id:\n" + str(imdb_id_url_mismatches))
 
     def testAtLeastOneImageForEach(self):
         """
@@ -32,7 +67,24 @@ class ImageCountTestCases(unittest.TestCase):
         for el in self.json_dict:
             if el['count'] < 1:
                 below_zero_elements.append(el)
-        self.assertEquals(below_zero_elements,[])
-        
+        self.assertEquals(below_zero_elements,
+                            [],
+                            msg="The following dicationaries had counts below 0:\n" + str(below_zero_elements))
+
+    def testValidImdbUrl(self):
+        """
+            Description:    Checks to see if each imdb url is valid and resolves
+            Details:    Performs a GET on all IMDB urls.
+                        Collects all urls that do not respond with a 200 return code.
+        """
+        non_valid_urls = []
+        for el in self.json_dict:
+            response = requests.get(el['url'])
+            if response.status_code != 200:
+                non_valid_urls.append(el)
+        self.assertEquals(non_valid_urls,
+                            [],
+                            msg="The following dictionaries had urls that did not return 200s:\n" + str(non_valid_urls))
+
 if __name__ == '__main__':
     unittest.main()
